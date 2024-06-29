db4406ad74012ec599f92a1928d1ddc9a2c0d6dc
Elias Yalew <eliasyalew1997@gmail.com>
2024-06-29 15:33:12 +0300

    fix docker install package issue

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,6 +7,8 @@ import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { UsersService } from 'src/users/users.service';
+import { LocalStrategy } from './local.strateg';
 
 @Module({
   imports:[MongooseModule.forFeature([{name:"User",schema:UserSchema}]),
@@ -17,6 +19,6 @@ import { jwtConstants } from './constants';
   signOptions: { expiresIn: '60s' },
 })],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService,UsersService,LocalStrategy]
 })
 export class AuthModule {}
